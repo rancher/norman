7e97b52811371a503e555ff17aab2c645ed94b2a
rajashree <rajashree.28m@gmail.com>
2019-10-02 14:20:52 -0700

    Bump api-ui version to 1.1.7

--- api/writer/html.go
+++ api/writer/html.go
@@ -10,7 +10,7 @@ import (
 const (
 	JSURL          = "https://releases.rancher.com/api-ui/%API_UI_VERSION%/ui.min.js"
 	CSSURL         = "https://releases.rancher.com/api-ui/%API_UI_VERSION%/ui.min.css"
-	DefaultVersion = "1.1.6"
+	DefaultVersion = "1.1.7"
 )
 
 var (
